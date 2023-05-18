c00ee8da3469fd226db37608f3515478e1563ba0
wzx27 <1041396750@qq.com>
2023-05-18 21:54:01 +0800

    Fix typo and add more descriptions

--- gym/problems/bandit/baseline.py
+++ gym/problems/bandit/baseline.py
@@ -12,7 +12,15 @@ class Solution:
         random.seed(0)
 
     def take_action(self) -> int:
+        """
+        Choose an arm to pull.
+        @return: the index of the arm to pull
+        """
         return random.randint(0, self.k - 1)
 
     def on_feedback(self, reward: float) -> None:
+        """
+        Receive feedback from the environment after taking an action.
+        @param reward: the reward received
+        """
         pass
